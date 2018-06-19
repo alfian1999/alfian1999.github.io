40e83a53606a3c95fccf408c0ba16253e0f04162
alfian1999 <40378382+alfian1999@users.noreply.github.com>
2018-06-19 13:29:53 +0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,7 @@
+<ul class="nav nav-sidebar">
+	<li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+	<li><a href="index.php?page=jurusan">Jurusan</a></li>
+	<li><a href="#">Mata Kuliah</a></li>
+	<li><a href="#">Dosen</a></li>
+	<li><a href="#">Mahasiswa</a></li>
+</ul>
